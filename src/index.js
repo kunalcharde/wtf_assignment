85ea4e2a9c3c6ac1d235ae1747d4ee2908dab124
Kunal Charde <kunalcharde1@gmail.com>
2023-03-29 00:36:17 +0530

    *

--- src/index.js
+++ src/index.js
@@ -2,12 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { GymContextProvider } from './Context/gymContext';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <GymContextProvider>
     <App />
+    </GymContextProvider>
   </React.StrictMode>
 );
 
